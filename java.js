9f865a4fd6b22378a44cdb971ebce0d8a89fe7d8
Taufique <112570645+imdtaufique@users.noreply.github.com>
2024-06-30 14:55:02 +0300

    Update java.js

--- java.js
+++ java.js
@@ -1,7 +1,10 @@
-
 onload = () => {
-    const c = setTimeout(() => {
-      document.body.classList.remove("not-loaded");
-      clearTimeout(c);
-    }, 1000);
-  };
+  const c = setTimeout(() => {
+    document.body.classList.remove("not-loaded");
+    clearTimeout(c);
+  }, 1000);
+};
+
+document.getElementById('forgiveButton').addEventListener('click', function() {
+  alert('I hope you can forgive me. I love you.');
+});
